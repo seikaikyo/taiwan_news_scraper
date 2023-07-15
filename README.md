52338c24d377277c932804c4d894303a754c9ddb
Kyo SeiKai <phpbbtw@gmail.com>
2023-07-16 00:16:12 +0800

    Commit Title: Refactor code and integrate Taiwan and Japan news scraping into one file
    Commit Description:
    This commit consolidates the Taiwan and Japan news scraping functionality into a single file, `main.py`. This simplifies the program structure and makes it easier to manage.
    
    Changes made:
    1. Removed `taiwan_news_scraper.py` and `nhk_news_scraper.py`.
    2. Created `main.py` which handles news scraping for both Taiwan and Japan.
    3. Implemented a function, `get_news`, in `main.py` to collect and filter news from RSS feeds based on specified parameters.
    4. Updated the news display templates to accommodate changes in the news data structure.
    5. The description content for Japan news that exceeds 300px in height is now truncated to maintain a consistent display.
    6. Updated `README.md` to reflect these changes.

--- README.md
+++ README.md
@@ -3,48 +3,42 @@
 ![Preview](templates/preview.png)
 ![Preview](templates/nhk_preview.png)
 
-This program collects and aggregates news from various Taiwanese and Japanese news RSS feeds. It enables users to filter news based on specific keywords, adjust the display order of news items, and limit the number of news items displayed per feed. In addition, users can hide news from categories they are not interested in.
+This program collects and aggregates news from various Taiwanese and Japanese news RSS feeds. It enables users to filter news based on specific keywords, adjust the display order of news items, and limit the number of news items displayed per feed. In addition, users can hide news from categories they are not interested in. For any news description that exceeds a height of 300px, it is truncated and a "See More" link is provided for the full description.
 
 ## Usage
 
 1. Install the requirements by running `pip install Flask feedparser BeautifulSoup Flask-Bootstrap`.
-2. To collect and display Taiwan news, run `python taiwan_news_scraper.py`. This program uses the `news_template.html` file to structure the news.
-3. To collect and display Japan news, run `python nhk_news_scraper.py`. This program uses the `nhk_news_template.html` file to structure the news.
-
-Each program will generate an HTML file named news_output.html containing the news items aggregated from the specified RSS feeds.
+2. Run `python news_scraper.py`.
+3. Open your web browser and go to `http://localhost:5000`.
 
 ## Configuration
 
-You can modify the RSS feeds, exclusion keywords, lower-rank keywords, and hide categories in the `taiwan_news_scraper.py` and `nhk_news_scraper.py` files.
+You can modify the RSS feeds, exclusion keywords, lower-rank keywords, and hide categories in the news_scraper.py file.
 
 # 台湾・日本ニューススクレーパー
 
-このプログラムは、様々な台湾と日本のニュース RSS フィードからニュースを収集し、集約します。ユーザーは、特定のキーワードに基づいてニュースをフィルタリングし、ニュース項目の表示順序を調整し、フィードごとに表示されるニュース項目の数を制限することができます。さらに、ユーザーは興味のないカテゴリーのニュースを非表示にすることができます。
+このプログラムは、様々な台湾と日本のニュース RSS フィードからニュースを収集し、集約します。ユーザーは、特定のキーワードに基づいてニュースをフィルタリングし、ニュース項目の表示順序を調整し、フィードごとに表示されるニュース項目の数を制限することができます。さらに、ユーザーは興味のないカテゴリーのニュースを非表示にすることができます。また、高さが 300px を超えるニュースの説明は切り捨てられ、「詳細を見る」のリンクが全文の説明のために提供されます。
 
 ## 使用方法
 
-1. `pip install Flask feedparser BeautifulSoup Flask-Bootstrap` を実行して、必要なパッケージをインストールします。
-2. 台湾のニュースを収集して表示するには、`python taiwan_news_scraper.py` を実行します。このプログラムは `news_template.html` ファイルを使ってニュースを構成します。
-3. 日本のニュースを収集して表示するには、`python nhk_news_scraper.py` を実行します。このプログラムは `nhk_news_template.html` ファイルを使ってニュースを構成します。
-
-各プログラムは、指定した RSS フィードから集約したニュース項目を含む `news_output.html` という名前の HTML ファイルを生成します。
+1. `pip install Flask feedparser BeautifulSoup Flask-Bootstrap` を実行して、必要なパッケージをインストールします
+2. `python news_scraper.py`を実行します。
+3. ウェブブラウザを開き、`http://localhost:5000` にアクセスします。
 
 ## 設定
 
-`taiwan_news_scraper.py` ファイルと `nhk_news_scraper.py` ファイルで、RSS フィード、除外キーワード、ランクを下げるキーワード、隠すカテゴリーを変更することができます。
+`news_scraper.py` ファイルで、RSS フィード、除外キーワード、ランクを下げるキーワード、隠すカテゴリーを変更することができます。
 
 # 台灣新聞擷取器與日本新聞擷取器
 
-此程式從各種台灣與日本新聞 RSS 饋送中收集並聚合新聞。它允許使用者根據特定的關鍵詞過濾新聞、調整新聞項目的顯示順序、並限制每個饋送顯示的新聞項目數量。此外，使用者可以隱藏他們不感興趣的類別的新聞。
+此程式從各種台灣與日本新聞 RSS 饋送中收集並聚合新聞。它允許使用者根據特定的關鍵詞過濾新聞、調整新聞項目的顯示順序、並限制每個饋送顯示的新聞項目數量。此外，使用者可以隱藏他們不感興趣的類別的新聞。對於任何超過 300px 高度的新聞描述，它會被截斷，並提供一個 "查看更多" 的連結以供完整描述。
 
 ## 使用方法
 
 1. 透過執行 `pip install Flask feedparser BeautifulSoup Flask-Bootstrap` 安裝所需的套件。
-2. 若要收集並顯示台灣新聞，請執行 `python taiwan_news_scraper.py`。此程式使用 `news_template.html` 檔案來結構化新聞。
-3. 若要收集並顯示日本新聞，請執行 `python nhk_news_scraper.py`。此程式使用 `nhk_news_template.html` 檔案來結構化新聞。
-
-每個程式將生成一個名為 `news_output.html` 的 HTML 文件，其中包含從指定的 RSS 饋送聚合的新聞項目。
+2. 執行`python news_scraper.py`
+3. 在網頁瀏覽器中打開 `http://localhost:5000`.
 
 ## 配置
 
-您可以在 `taiwan_news_scraper.py` 及 `nhk_news_scraper.py` 文件中修改 RSS 饋送、排除關鍵詞、降低排名的關鍵詞和隱藏類別。
+您可以在 `news_scraper.py` 文件中修改 RSS 饋送、排除關鍵詞、降低排名的關鍵詞和隱藏類別。
---
+++ news_scraper.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template
+import feedparser
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from dateutil import parser
+import textwrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+# RSS feeds for different categories
+RSS_FEEDS_TAIWAN = {
+    '即時': 'https://news.ltn.com.tw/rss/all.xml',
+    '國際': 'https://news.ltn.com.tw/rss/world.xml',
+    '財經': 'https://news.ltn.com.tw/rss/business.xml',
+    '生活': 'https://news.ltn.com.tw/rss/life.xml',
+    '社會': 'https://news.ltn.com.tw/rss/society.xml',
+    '地方': 'https://news.ltn.com.tw/rss/local.xml',
+    '蒐奇': 'https://news.ltn.com.tw/rss/novelty.xml',
+    '娛樂': 'https://news.ltn.com.tw/rss/entertainment.xml',
+    '評論': 'https://news.ltn.com.tw/rss/opinion.xml',
+    '政治': 'https://news.ltn.com.tw/rss/politics.xml',
+    '體育': 'https://news.ltn.com.tw/rss/sports.xml'
+}  # Taiwan RSS feeds
+
+RSS_FEEDS_JAPAN = {
+    '主要ニュース': 'https://www3.nhk.or.jp/rss/news/cat0.xml',
+    '社会': 'https://www3.nhk.or.jp/rss/news/cat1.xml',
+    '科学.医療': 'https://www3.nhk.or.jp/rss/news/cat3.xml',
+    '政治': 'https://www3.nhk.or.jp/rss/news/cat4.xml',
+    '経済': 'https://www3.nhk.or.jp/rss/news/cat5.xml',
+    '国際': 'https://www3.nhk.or.jp/rss/news/cat6.xml',
+    'スポーツ': 'https://www3.nhk.or.jp/rss/news/cat7.xml',
+    '文化.エンタメ': 'https://www3.nhk.or.jp/rss/news/cat2.xml',
+}  # Japan RSS feeds
+
+# Keywords for excluding or lowering rank
+EXCLUDE_KEYWORDS_TAIWAN = {"中國", "中英對照讀新聞", "中職", "民眾黨", "濕身", "郭台銘",
+                           "廖大乙", "侯友宜", "中醫", "民俗", "證券行情表", "證券表格",
+                           "浪浪", "環保團體", "自由說新聞", "官我什麼事", "謠言終結站",
+                           "飆股幕後", "首長早餐會", "王力宏", "Makiyo", "弦子", "吳鳳",
+                           "彭佳慧", "MLB", "金廈", "如懿傳", "黃國昌", "館長", "李玟",
+                           "亞錦", "張秀卿", "股市", "柯志恩", "周子瑜", "聯賽", "游淑慧",
+                           "王世堅", "高嘉瑜", "林心如", "柯文哲", "亞運", "男籃", "演藝圈",
+                           "高虹安", "選秀", "TIME", "盧秀燕", "韓國瑜",
+                           "朱立倫", "馬英九", "素食", "小甜甜", "TikTok", "戴愛玲", "何志偉",
+                           "客家", "日職", "539", "國際油價", "黃金", "盧廣仲", "大愛", "全裸",
+                           "裸奔", "詐騙", "捐血", "小巨蛋", "陳奕迅"}
+LOWER_RANK_KEYWORDS_TAIWAN = {"羨慕", "熱情"}
+
+# Categories to hide
+HIDE_CATEGORIES_TAIWAN = {"政治", "娛樂", "體育", "地方", "評論", "蒐奇"}
+
+# Keywords for excluding or lowering rank
+EXCLUDE_KEYWORDS_JAPAN = {"中國"}
+LOWER_RANK_KEYWORDS_JAPAN = {"情熱"}
+
+# Categories to hide
+HIDE_CATEGORIES_JAPAN = {"スポーツ", "政治", "文化.エンタメ"}
+
+NEWS_PER_FEED = 20  # Maximum number of news items per feed
+MAX_PARAGRAPHS = 3  # Maximum number of paragraphs to include in the summary
+
+
+def trim_description(description, max_lines=10, width=80):
+    """Trim the description to a certain number of lines."""
+    lines = textwrap.wrap(description, width=width)
+    if len(lines) > max_lines:
+        lines = lines[:max_lines]
+    return ' '.join(lines)
+
+
+def get_news(RSS_FEEDS, EXCLUDE_KEYWORDS, LOWER_RANK_KEYWORDS, HIDE_CATEGORIES):
+    all_articles = {}
+    seen_articles = set()  # Track seen articles
+
+    for category, url in RSS_FEEDS.items():
+        # Skip categories that we want to hide
+        if category in HIDE_CATEGORIES:
+            continue
+
+        feed = feedparser.parse(url)
+        filtered_articles = []
+        for entry in feed.entries:
+            # Skip articles that contain excluded keywords in title or description
+            if any(keyword in entry.title for keyword in EXCLUDE_KEYWORDS) or \
+                    any(keyword in entry.description for keyword in EXCLUDE_KEYWORDS):
+                continue
+
+            title = entry.title
+            link = entry.link
+            description = trim_description(entry.description)
+            published_time = entry.published
+            # Get the published time of the article
+            published_time = parser.parse(entry.published)
+
+            formatted_published_time = published_time.strftime(
+                '%Y-%m-%d %H:%M:%S')
+
+            # Generate unique ID for the article
+            article_id = hash((title, description))
+            if article_id in seen_articles:  # Skip if the article has been seen before
+                continue
+            seen_articles.add(article_id)
+
+            filtered_article = {
+                'title': title,
+                'link': link,
+                'description': description,
+                'published_time': formatted_published_time
+            }
+
+            # If the article contains a lower-rank keyword, add it to the end
+            if any(keyword in filtered_article['title'] for keyword in LOWER_RANK_KEYWORDS):
+                filtered_articles.append(filtered_article)
+            else:
+                # Otherwise, add it to the front
+                filtered_articles.insert(0, filtered_article)
+
+        # Limit the articles to NEWS_PER_FEED
+        all_articles[category] = filtered_articles[:NEWS_PER_FEED]
+    return all_articles
+
+
+@app.route('/')
+def news():
+    all_articles_taiwan = get_news(
+        RSS_FEEDS_TAIWAN, EXCLUDE_KEYWORDS_TAIWAN, LOWER_RANK_KEYWORDS_TAIWAN, HIDE_CATEGORIES_TAIWAN)
+    all_articles_japan = get_news(
+        RSS_FEEDS_JAPAN, EXCLUDE_KEYWORDS_JAPAN, LOWER_RANK_KEYWORDS_JAPAN, HIDE_CATEGORIES_JAPAN)
+    return render_template('news_template.html', taiwan_articles=all_articles_taiwan, japan_articles=all_articles_japan)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
